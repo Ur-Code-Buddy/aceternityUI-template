48886b200d90fad09ae2c9ae274d88588f160767
Ur-Code-Buddy <05baivab@gmail.com>
2024-07-26 23:29:04 +0530

    Aceternity UI is working

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { CardBody, CardContainer, CardItem } from "./components/ui/3d-card";
+import { FlipWords } from "./components/ui/flip-words";
+import {LampDemo} from "./components/ui/lamp";
+function App() {
+
+  return (
+    <div>
+      <CardContainer>
+        <CardBody>
+          <CardItem>
+            
+            <img src='https://plus.unsplash.com/premium_photo-1675252369719-dd52bc69c3df?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+          </CardItem>
+        </CardBody>
+      </CardContainer>
+    </div>
+  );
+  
+}
+
+export default App;
